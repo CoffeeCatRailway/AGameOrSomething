26cbcc6895e4fe117ae20bcb8b261a96ce58dd40
CoffeeCatRailway <dragonlee1002@gmail.com>
2022-11-25 09:28:34 +1100

    :bug: Fixed 'stripes'

--- src/main/java/io/github/coffeecatrailway/agameorsomething/client/render/texture/AtlasEntry.java
+++ src/main/java/io/github/coffeecatrailway/agameorsomething/client/render/texture/AtlasEntry.java
@@ -11,6 +11,8 @@ import org.joml.Vector4fc;
  */
 public class AtlasEntry
 {
+    private static final float PADDING = 0.001f; // Prevents "stripes" appearing
+
     private final ObjectLocation id;
     private final Vector4fc uvCoords;
     private final TextureAtlas<?> atlas;
@@ -19,7 +21,10 @@ public class AtlasEntry
     {
         this.id = id;
         this.atlas = atlas;
-        this.uvCoords = new Vector4f((float) x / (float) atlas.getWidth(), (float) y / (float) atlas.getHeight(), (float) width / (float) atlas.getWidth(), (float) height / (float) atlas.getHeight());
+        this.uvCoords = new Vector4f((float) x / (float) atlas.getWidth() + PADDING,
+                (float) y / (float) atlas.getHeight() + PADDING,
+                (float) width / (float) atlas.getWidth() - PADDING * 2f,
+                (float) height / (float) atlas.getHeight() - PADDING * 2f);
     }
 
     public AtlasEntry(JsonObject json, TextureAtlas<?> atlas)
